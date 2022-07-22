cd02b3bb8531e289497fede317107675f47de75c
VishnuMS13 <105206650+VishnuMS13@users.noreply.github.com>
2022-07-22 12:53:12 +0530

    Add files via upload

---
+++ typewise-alert.cpp
@@ -0,0 +1,75 @@
+#include "typewise-alert.hpp"
+
+BreachType inferBreach(double value, double lowerLimit, double upperLimit) {
+  BreachType returnValue;
+  if(value < lowerLimit) {
+    returnValue = TOO_LOW;
+  } else if(value > upperLimit) {
+    returnValue = TOO_HIGH;
+  } else {
+    returnValue = NORMAL;
+  }
+  return returnValue;
+}
+
+BreachType classifyTemperatureBreach(
+    CoolingType coolingType, double temperatureInC) {
+  int lowerLimit = 0;
+  int upperLimit = 0;
+  switch(coolingType) {
+    case PASSIVE_COOLING:
+      lowerLimit = 0;
+      upperLimit = 35;
+      break;
+    case HI_ACTIVE_COOLING:
+      lowerLimit = 0;
+      upperLimit = 45;
+      break;
+    case MED_ACTIVE_COOLING:
+      lowerLimit = 0;
+      upperLimit = 40;
+      break;
+    default:
+      break;
+  }
+  return inferBreach(temperatureInC, lowerLimit, upperLimit);
+}
+
+void checkAndAlert(
+    AlertTarget alertTarget, BatteryCharacter batteryChar, double temperatureInC) {
+
+  BreachType breachType = classifyTemperatureBreach(
+    batteryChar.coolingType, temperatureInC
+  );
+
+  switch(alertTarget) {
+    case TO_CONTROLLER:
+      sendToController(breachType);
+      break;
+    case TO_EMAIL:
+      sendToEmail(breachType);
+      break;
+  }
+}
+
+void sendToController(BreachType breachType) {
+  const unsigned short header = 0xfeed;
+  cout << header << ":" << breachType << endl;
+}
+
+void sendToEmail(BreachType breachType) {
+  const char* recepient = "ms.vishnu@gmail.com";
+  switch(breachType) {
+    case TOO_LOW:
+      cout << "To: " << recepient << endl;
+      cout << "Hi, the temperature is too low" << endl;
+      break;
+    case TOO_HIGH:
+      cout << "To: " << recepient << endl;
+      cout << "Hi, the temperature is too High" << endl;
+      break;
+    case NORMAL:
+      break;
+  }
+}
+
---
+++ typewise-alert.hpp
@@ -0,0 +1,41 @@
+#pragma once
+
+#include <stdio.h>
+#include <iostream>
+#include <sstream>
+using namespace std;
+
+#define LOW_VALUE  -5
+#define MID_VALUE  22
+#define HIGH_VALUE  50
+
+typedef enum {
+  PASSIVE_COOLING,
+  HI_ACTIVE_COOLING,
+  MED_ACTIVE_COOLING
+} CoolingType;
+
+typedef enum {
+  NORMAL,
+  TOO_LOW,
+  TOO_HIGH
+} BreachType;
+
+BreachType inferBreach(double value, double lowerLimit, double upperLimit);
+BreachType classifyTemperatureBreach(CoolingType coolingType, double temperatureInC);
+
+typedef enum {
+  TO_CONTROLLER,
+  TO_EMAIL
+} AlertTarget;
+
+typedef struct {
+  CoolingType coolingType;
+  char brand[48];
+} BatteryCharacter;
+
+void checkAndAlert(
+  AlertTarget alertTarget, BatteryCharacter batteryChar, double temperatureInC);
+
+void sendToController(BreachType breachType);
+void sendToEmail(BreachType breachType);
